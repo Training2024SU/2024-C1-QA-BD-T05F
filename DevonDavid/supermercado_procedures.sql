08ff3f985e7bf72fbfb5d91675ec5fdfe477d998
Devon Alvarez <osorioalvarez20@gmail.com>
2024-04-02 18:56:10 -0500

    Se agregan procedimientos almacenado insertar y eliminar producto

--- DevonDavid/supermercado_procedures.sql
+++ DevonDavid/supermercado_procedures.sql
@@ -1,7 +1,8 @@
 -- Stored procedures supermercado
 USE supermercado;
 
--- DROP PROCEDURE pendientes_repartidor;
+-- DROP PROCEDURE eliminar_producto;
+-- DROP PROCEDURE insertar_producto;
 -- DROP PROCEDURE productos_en_categoria;
 -- DROP PROCEDURE pedido_con_valor;
 
@@ -13,14 +14,33 @@ BEGIN
 	SELECT * FROM Productos p WHERE p.nombre_categoria = nombre_categoria;
 END $$
 -- Ejemplo:
-CALL productos_en_categoria("congelados") $$
+CALL productos_en_categoria("alimentos") $$
 
--- Listado de pedidos de un repartidor pendientes por repartir
-CREATE PROCEDURE pendientes_repartidor(IN id_repartidor INT)
+-- ---------------------------------------------
+-- Insertar un nuevo producto
+-- ---------------------------------------------
+DELIMITER //
+CREATE PROCEDURE insertar_producto(
+	IN id INT,
+    IN nombre_producto VARCHAR(50),
+    IN marca_producto VARCHAR(40),
+    IN origen_producto VARCHAR(40),
+    IN volumen_producto VARCHAR(20),
+    IN peso_producto VARCHAR(20),
+    IN fotografia_producto VARCHAR(255),
+    IN precio_producto BIGINT,
+    IN stock_producto BIGINT,
+    IN categoria_producto VARCHAR(40)
+)
 BEGIN
+    INSERT INTO Productos (id,nombre, marca, origen, volumen, peso, fotografia, precio, stock, nombre_categoria)
+    VALUES (id,nombre_producto, marca_producto, origen_producto, volumen_producto, peso_producto, fotografia_producto, precio_producto, stock_producto, categoria_producto);
+END //
+DELIMITER ;
 
-END $$
+CALL insertar_producto('12','botella de Agua','Brisa','Colombia','20ml','200g','botella_de_agua.jpg','1500','30','Bebidas');
 
+DELIMITER //
 -- Detalles de pedido con importe total
 CREATE PROCEDURE pedido_con_valor(IN id_pedido INT)
 BEGIN
@@ -36,15 +56,22 @@ BEGIN
 		Clientes c ON p.id_cliente = c.id
 	GROUP BY p.id;
 END $$
-CALL pedido_con_valor(1) $$
-
--- Crear almacenista con una area
-CREATE PROCEDURE crear_almacenista_con_area (IN id INT, IN nombre VARCHAR(50), IN area VARCHAR(20))
-BEGIN
-	INSERT INTO Almacenistas VALUES (id, nombre);
-    INSERT INTO almacenistas_areas VALUES (id, area);
-END $$
-
-
 DELIMITER ;
+
+CALL pedido_con_valor(1);
+
+-- -----------------------------------------------------------
+-- eliminar producto de la tabla productos
+-- -----------------------------------------------------------
+DELIMITER //
+CREATE PROCEDURE eliminar_producto(
+    IN id_producto INT
+)
+BEGIN
+    DELETE FROM Productos
+    WHERE id = id_producto;
+END //
+DELIMITER ;
+
+CALL eliminar_producto(12);
 
