19a3f36a94924fa7317c026bf5ac1b3a6dbc2053
Juan Jose <idkb1910@gmail.com>
2024-04-04 18:41:50 -0300

    Se hacen correcciones en la definicion del database

--- ZOO/Database_definition.sql
+++ ZOO/Database_definition.sql
@@ -3,6 +3,8 @@
 -- Host: localhost    Database: zoologico
 -- ------------------------------------------------------
 -- Server version	8.0.36
+CREATE SCHEMA IF NOT EXISTS `Zoologico` DEFAULT CHARACTER SET utf8 ;
+USE `Zoologico` ;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
